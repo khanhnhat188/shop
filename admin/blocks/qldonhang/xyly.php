cce1d0af056e2116763c57fba84d48f43fdd4411
khanhnhat188 <teamka71@gmail.com>
2023-12-10 20:13:23 +0700

    time

--- admin/blocks/qldonhang/xyly.php
+++ admin/blocks/qldonhang/xyly.php
@@ -1,9 +1,22 @@
 <?php
+    // Đảm bảo kết nối được thiết lập trước khi sử dụng
     include '../../connect/conn.php';
-    if(isset($_GET['trangthai'])){
+
+    if(isset($_GET['code'])){
+        // Lấy mã đơn hàng từ tham số truyền vào
         $code = $_GET['code'];
+
+        // Cập nhật trạng thái của đơn hàng thành 'Chưa xử lý'
         $sql_update = "UPDATE donhang SET trangthai = 0 WHERE madonhang = '$code'";
-        mysqli_query($conn, $sql_update);
-        header('Location: ../index.php?action=donhang&query=lietke');
+        $result_update = mysqli_query($conn, $sql_update);
+
+        // Kiểm tra xem cập nhật thành công hay không trước khi chuyển hướng
+        if ($result_update) {
+            // Chuyển hướng người dùng đến trang liệt kê đơn hàng
+            header('Location: ../../index.php?action=qldonhang&query=lietke');
+        } else {
+            // Xử lý lỗi nếu cập nhật không thành công
+            echo 'Có lỗi xảy ra khi cập nhật trạng thái đơn hàng.';
+        }
     }
 ?>
--- admin/index.php
+++ admin/index.php
@@ -10,25 +10,34 @@ include 'connect/conn.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/admin.css">
     <link rel="icon" type="image/x-icon" href="../img/favicon.ico">
+    <!-- Morris.js CSS -->
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+
     <title>Admin</title>
 </head>
+
 <body>
     <h3 class="title_admin">QUẢN TRỊ</h3>
     <div class="wrapper">
         <?php
-            include "blocks/header.php";
-            include "blocks/menu.php";
-            include "blocks/main.php";
-        ?>
+                include "blocks/header.php";
+                include "blocks/menu.php";
+                include "blocks/main.php";
+            ?>
     </div>
 
-</div>
-</div>
+
+
 </body>
+
 </html>
